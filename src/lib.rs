5a3e17be8271eff3ae52cd89f441fb75565c37c1
Akishika Issui <kshkss@users.noreply.github.com>
2021-03-14 00:19:53 +0900

    Add python interface for complex array

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use num_complex::Complex64;
 use numpy::{IntoPyArray, PyArray1, PyArray2, PyReadonlyArray1};
 use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
 
@@ -6,8 +7,8 @@ pub mod berg_f64;
 
 #[pymodule]
 fn rust_ext(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    #[pyfn(m, "reflection_coeff")]
-    fn reflection_coeff_py<'py>(
+    #[pyfn(m, "reflection_coeff_f64")]
+    fn reflection_coeff_f64<'py>(
         py: Python<'py>,
         max_lag: usize,
         y: PyReadonlyArray1<f64>,
@@ -16,8 +17,8 @@ fn rust_ext(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         berg_f64::reflection_coeff(max_lag, &y).into_pyarray(py)
     }
 
-    #[pyfn(m, "variance")]
-    fn variance_py<'py>(
+    #[pyfn(m, "variance_f64")]
+    fn variance_f64<'py>(
         py: Python<'py>,
         gamma: PyReadonlyArray1<f64>,
         y: PyReadonlyArray1<f64>,
@@ -27,8 +28,8 @@ fn rust_ext(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         berg_f64::variance(&gamma, &y).into_pyarray(py)
     }
 
-    #[pyfn(m, "power_spector")]
-    fn power_spector_py<'py>(
+    #[pyfn(m, "power_spector_f64")]
+    fn power_spector_f64<'py>(
         py: Python<'py>,
         dt: f64,
         p: f64,
@@ -38,6 +39,40 @@ fn rust_ext(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         let gamma = gamma.as_array();
         let q = q.as_array();
         berg_f64::power_spector(dt, p, &gamma, &q).into_pyarray(py)
+    }
+
+    #[pyfn(m, "reflection_coeff_c64")]
+    fn reflection_coeff_c64<'py>(
+        py: Python<'py>,
+        max_lag: usize,
+        y: PyReadonlyArray1<Complex64>,
+    ) -> &'py PyArray2<Complex64> {
+        let y = y.as_array();
+        berg_c64::reflection_coeff(max_lag, &y).into_pyarray(py)
+    }
+
+    #[pyfn(m, "variance_c64")]
+    fn variance_c64<'py>(
+        py: Python<'py>,
+        gamma: PyReadonlyArray1<Complex64>,
+        y: PyReadonlyArray1<Complex64>,
+    ) -> &'py PyArray1<f64> {
+        let gamma = gamma.as_array();
+        let y = y.as_array();
+        berg_c64::variance(&gamma, &y).into_pyarray(py)
+    }
+
+    #[pyfn(m, "power_spector_c64")]
+    fn power_spector_c64<'py>(
+        py: Python<'py>,
+        dt: f64,
+        p: f64,
+        gamma: PyReadonlyArray1<Complex64>,
+        q: PyReadonlyArray1<f64>,
+    ) -> &'py PyArray1<f64> {
+        let gamma = gamma.as_array();
+        let q = q.as_array();
+        berg_c64::power_spector(dt, p, &gamma, &q).into_pyarray(py)
     }
 
     Ok(())
